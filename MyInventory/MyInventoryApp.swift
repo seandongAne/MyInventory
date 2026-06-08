2b41a244b94463da5520a6828daffa2f8495d03a
Sean Dong <seandong1993@gmail.com>
2026-06-08 01:11:24 -0400

    Initial Commit

---
+++ MyInventory/MyInventoryApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyInventoryApp.swift
+//  MyInventory
+//
+//  Created by Sean Dong on 2026-06-08.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyInventoryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
